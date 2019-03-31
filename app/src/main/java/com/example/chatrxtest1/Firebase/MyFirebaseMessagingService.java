db5b898c4e48721f004eba097acd5ccd0467b065
DavidLif <davidshani12@gmail.com>
2019-03-31 20:05:04 +0300

    commit before package rename for new firebase

--- app/src/main/java/com/example/chatrxtest1/Firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/chatrxtest1/Firebase/MyFirebaseMessagingService.java
@@ -1,6 +1,7 @@
 package com.example.chatrxtest1.Firebase;
 
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,33 +15,45 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated.
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.d(TAG, "To: " + remoteMessage.getTo());
-        Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+        try {
+            // If the application is in the foreground handle both data and notification messages here.
+            // Also if you intend on generating your own notifications as a result of a received FCM
+            // message, here is where that should be initiated.
+            Log.d(TAG, "From: " + remoteMessage.getFrom());
+            Log.d(TAG, "To: " + remoteMessage.getTo());
+            if (remoteMessage.getNotification() != null) {
+                if (remoteMessage.getNotification().getBody() == null) {
+                    Log.d(TAG, "Notification Message Body: Null");
+                }
+                else {
+                    Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+                }
+            }
 
-        Intent intent = new Intent();
-        Map<String, String> msgMetaData =  remoteMessage.getData();
-        if (msgMetaData.containsKey("SourceId")) {
-            intent.putExtra("SourceId", msgMetaData.get("SourceId"));
+            Intent intent = new Intent();
+            Map<String, String> msgMetaData =  remoteMessage.getData();
+            if (msgMetaData.containsKey("SourceId")) {
+                intent.putExtra("SourceId", msgMetaData.get("SourceId"));
+            }
+            if (msgMetaData.containsKey("Content")) {
+                intent.putExtra("Content", msgMetaData.get("Content"));
+            }
+            if (msgMetaData.containsKey("MessageId")) {
+                intent.putExtra("MessageId", msgMetaData.get("MessageId"));
+            }
+
+            intent.setAction("com.example.chatrxtest1.onMessageReceived");
+            //LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(intent);
+            sendStickyBroadcast(intent); //sendStickyBroadcast use sticky broadcast so we can receive the intent even if the application is in the background
         }
-        if (msgMetaData.containsKey("Content")) {
-            intent.putExtra("Content", msgMetaData.get("Content"));
-        }
-        if (msgMetaData.containsKey("MessageId")) {
-            intent.putExtra("MessageId", msgMetaData.get("MessageId"));
+        catch (Exception exp) {
+            Log.e(TAG, "Exception at firebase messaging android service", exp);
         }
 
-        intent.setAction("com.example.chatrxtest1.onMessageReceived");
-        sendStickyBroadcast(intent); //sendStickyBroadcast use sticky broadcast so we can receive the intent even if the application is in the background
     }
 
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
-        Log.d(TAG,String.format(""));
-
     }
 }
